99b8a30d8d714c4652498732303fd33f22d547d9
joshin49 <joshin97@naver.com>
2020-05-04 02:08:48 +0900

    first commit

---
+++ 2D_UI_Class_Code/Assets/Scripts/Button_Script.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button_Script : MonoBehaviour
+{
+
+    public static bool bCheck_Parameter = true;
+
+    public static int bHardware = 0;
+
+
+    [SerializeField] private InputField Name_Input;
+
+    [SerializeField] private InputField Password_Input;
+
+    [SerializeField] private Slider Slider_Value;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void My_Button()
+    {
+        Debug.Log("Hi I am a button");
+    }
+
+    public void MyCheckButton()
+    {
+        if (bCheck_Parameter == true)
+        {
+            bCheck_Parameter = false;
+
+        }else if (bCheck_Parameter == false)
+        {
+            bCheck_Parameter = true;
+        }
+
+        Debug.Log("My check button:" + bCheck_Parameter);
+    }
+
+    public void MyRadioButton1()
+    {
+        bHardware = 1;
+        Debug.Log("오픈지엘을 선택했습니다:" + bHardware);
+    }
+    public void MyRadioButton2()
+    {
+        bHardware = 2;
+        Debug.Log("다이렉트를 선택했습니다:" + bHardware);
+    }
+
+    public void MyInputField_Name()
+    {
+        Debug.Log("이름을 입력했습니다.");
+        if(Name_Input.text == "Kang")
+        {
+            Debug.Log("존재하는 ID 입니다");
+        }
+    }
+    public void MyInputField_Password()
+    {
+        Debug.Log("암호를 입력했습니다.");
+        if (Password_Input.text == "1111")
+        {
+            Debug.Log("암호가 일치합니다.");
+        }
+    }
+
+    public void MySlider()
+    {
+        Debug.Log("슬라이더값:" + Slider_Value.value);
+    }
+}
---
+++ 2D_UI_Class_Code/Assets/Scripts/UI_Manager_Script.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UI_Manager_Script : MonoBehaviour
+{
+    
+
+    [SerializeField] private InputField Name_Input;
+
+    [SerializeField] private InputField Password_Input;
+
+    [SerializeField] private Slider Slider_Value;
+
+    //[SerializeField] private InputField input_name;
+
+    public Slider my_slider;
+
+
+    public static bool bFlip;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bFlip = true;
+    }
+
+    // Update is called once per frame
+    
+
+    
+
+   
+
+    public void Myslider()
+    {
+        Debug.Log("slider Value: " + my_slider.value);
+    }
+    
+
+
+    void MyButton_in_UIManager()
+    {
+        Debug.Log("button 1 pressed by UI Manager");
+    }
+
+    
+    
+
+    
+}
